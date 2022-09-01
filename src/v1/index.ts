6ee9209a819305c94e20810e6452c2e603b2c7bb
gorden <limguancheng@hotmail.com>
2022-09-01 18:00:50 +0800

    add auth

--- src/v1/index.ts
+++ src/v1/index.ts
@@ -2,13 +2,40 @@ import express from 'express';
 
 const router = express.Router();
 
+// sample calling other services
+// import { GoogleAuth } from 'google-auth-library';
+// import { IResponse } from '../interfaces/response.interface';
+// const auth = new GoogleAuth();
+// router.post('/:service', async (req, res) => {
+//   const service = req.params.service;
+//   const basedUrl = 'https://service-abc-uc.a.run.app';
+//   const url = `${basedUrl}/v1/${service}`;
+//   try {
+//     const client = await auth.getIdTokenClient(`${basedUrl}/`);
+//     const result = await client.request<IResponse>({
+//       url: url,
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       method: 'POST',
+//       data: req.body,
+//       responseType: 'json',
+//     });
+//     res.status(result.status).send({ message: result.data.message, status: 'success', data: result.data.data });
+//   } catch (error: any) {
+//     console.error(error);
+//     res.status(500).send({ message: error.message, status: 'error' });
+//   }
+// });
+
+
 // create
 router.post('/', async (req, res) => {
   const body = req.body;
   try {
     res.status(200).send({ message: 'success', status: 'success', data: body });
   } catch (error: any) {
-    res.status(500).send({ message: error.message, status: 'error' });
+    res.status(500).send({ message: error.message, status: 'error', data: error });
   }
 });
 
@@ -18,7 +45,7 @@ router.get('/:id', async (req, res) => {
   try {
     res.status(200).send({ message: 'success', status: 'success', data: { id: id } });
   } catch (error: any) {
-    res.status(500).send({ message: error.message, status: 'error' });
+    res.status(500).send({ message: error.message, status: 'error', data: error });
   }
 });
 
@@ -28,7 +55,7 @@ router.post('/get_by', async (req, res) => {
   try {
     res.status(200).send({ message: 'success', status: 'success', data: body });
   } catch (error: any) {
-    res.status(500).send({ message: error.message, status: 'error' });
+    res.status(500).send({ message: error.message, status: 'error', data: error });
   }
 });
 
@@ -38,7 +65,7 @@ router.put('/', async (req, res) => {
   try {
     res.status(200).send({ message: 'success', status: 'success', data: body });
   } catch (error: any) {
-    res.status(500).send({ message: error.message, status: 'error' });
+    res.status(500).send({ message: error.message, status: 'error', data: error });
   }
 });
 
@@ -48,7 +75,7 @@ router.delete('/:id', async (req, res) => {
   try {
     res.status(200).send({ message: 'success', status: 'success', data: { id: id } });
   } catch (error: any) {
-    res.status(500).send({ message: error.message, status: 'error' });
+    res.status(500).send({ message: error.message, status: 'error', data: error });
   }
 });
 
